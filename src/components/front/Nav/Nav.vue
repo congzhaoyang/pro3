d98b74119045865daeded07488f7cbb127c60d65
congzhaoyang <congzhaoyang1@163.com>
2018-01-27 23:48:56 +0800

    [udpate]

---
+++ src/components/front/Nav/Nav.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-row>
+    <el-col :span="24">
+      <div class="grid-content bg-purple-dark">
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">文章</el-menu-item>
+          <el-menu-item index="3">书架</el-menu-item>
+          <el-menu-item index="5">相册</el-menu-item>
+          <el-menu-item index="6">时间线</el-menu-item>
+          <el-menu-item index="7">关于</el-menu-item>
+          <!-- <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item> -->
+        </el-menu>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',        
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
